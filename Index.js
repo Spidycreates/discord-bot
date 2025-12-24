144700acbb448bbfc96169a3f7375816ecc71f65
Spidycreates <pokeayush95@gmail.com>
2025-12-24 14:52:18 +0530

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,27 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", message => {
+  if (message.author.bot) return;
+
+  if (message.content === "!ping") {
+    message.reply("Hey master 👋");
+  }
+
+  if (message.content === "!hello") {
+    message.reply("Hello there!");
+  }
+});
+
+client.login(process.env.TOKEN);
